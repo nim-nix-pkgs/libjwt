2c42c3d50c10f71833122940e6e9fb4b78ec287c
hugosenari <hugosenari@gmail.com>
2022-03-27 18:19:31 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-libjwt-master.ref   = "refs/heads/master";
   inputs.src-libjwt-master.owner = "nimscale";
   inputs.src-libjwt-master.repo  = "nim-libjwt";
+  inputs.src-libjwt-master.dir   = "";
   inputs.src-libjwt-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
